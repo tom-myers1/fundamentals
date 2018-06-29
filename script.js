df63a7477fb2b1cf0dd23217e7c01869b10462a6
Tom Myers <tom.myers310@gmail.com>
2018-06-29 19:42:45 +0100

    basic functions

--- script.js
+++ script.js
@@ -2,26 +2,28 @@
 // using jscs linter
 //using jshint
 
-//alert('this is from the script file');
+// basic alert
+// alert('this is from the script file');
 var x = 10;
 var y = 11;
 
-//arrays have indexed values - can be strings, int other arrays etc
+// basic arrays
+// arrays have indexed values - can be strings, int other arrays etc
 var array = [];
 array[0] = 'hello';
 array[1] = 'world';
 
 var testarray = array[0] + ' ' + array[1];
 
-//objects key value pairs
+// basic objects
+// objects key value pairs
 var myobj = {
   first: 'hello',
   second: 'world',
   third: 50,
 };
 
-// testing arrays
-//alert(myobj.first + ' '  + myobj.second);
+// alert(myobj.first + ' '  + myobj.second);
 
 // my details object
 var mydetails = {
@@ -29,10 +31,72 @@ var mydetails = {
   secondname: 'Myers',
   age: 36,
 };
-alert(
+
+/* alert(
   'hello, my name is: ' +
     mydetails.firstname +
     ' '  +
     mydetails.secondname +
     '. learning javascript at ' +
     mydetails.age);
+*/
+
+// functions
+// block of executable code
+function functionOne() {
+  console.log('yo');
+
+  //js always wants to return something
+  return 'yo';
+}
+
+functionOne();
+
+function addStuff(a, b) {
+  return a + b;
+}
+
+addStuff(10, 25);
+
+// function scope
+//
+var a = 1; // global
+
+// global can be used here
+
+function changeLocal() {
+
+  // global can be used here
+
+  var a = 2; // local
+
+  // local a now = 2
+
+  return a;
+}
+
+// if you call a again it will be back to 1
+// changeLocal is a child of the main script
+// children have access to parental variables
+// but parents dont always have access to child variables
+
+function changeGlobal() {
+
+  // global still = 1
+
+  a = 2;
+
+  // global now = 2 because you are not creating a new variable
+  // just redefining a
+  // a instead of var a
+
+  return a;
+}
+
+// keep as much out of global scope as possible!!
+
+b = changeLocal();
+
+// so a would still = a
+// and b would == return value from changeLocal
+// which would be 2
